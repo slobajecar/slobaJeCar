ec38c0aaf289964a1fc0cfdb29c01220fabcba65
Slobodan Stanisic <stanisic.slobodan@gmail.com>
2011-06-25 22:11:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlobaJeCar::Application.config.secret_token = '3e7870757124d93a49d9dba15f83ffe5702f987b0f3834710c0e207362cc221d44528fe4aeb99e53c4c36d7f351af05b84f48a73621d854a61dad369d9470076'
